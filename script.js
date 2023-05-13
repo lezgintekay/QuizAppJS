ce13cd240e38cb4e194af0474cb08c85e6772feb
Lezgin Tekay <tekay.lezgin@yandex.com>
2023-05-13 03:12:41 +0300

    questions are now displayed dynamically

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ Soru.prototype.cevabiKontrolEt = function (cevap) {
 
 let sorular = 
 [
-    new Soru("Hangisi js paket yönetim uygulamasıdır?",{a: "Node.js", b: "TypeScrript", c: "npm"}, "c" ),
+    new Soru("Hangisi js paket yönetim uygulamasıdır?",{a: "Node.js", b: "TypeScrript", c: "npm", d: "nuget"}, "c" ),
     new Soru("Hangisi bles paket yönetim uygulamasıdır?",{a: "Node.js", b: "TypeScrript", c: "npm"}, "c" ),
     new Soru("Hangisi kles paket yönetim uygulamasıdır?",{a: "Node.js", b: "TypeScrript", c: "npm"}, "c" ),
     new Soru("Hangisi cles paket yönetim uygulamasıdır?",{a: "Node.js", b: "TypeScrript", c: "npm"}, "c" )
@@ -37,16 +37,40 @@ Quiz.prototype.soruGetir = function() {
 const quiz = new Quiz(sorular) ;
 
 document.querySelector(".btn_start").addEventListener("click" , function(){
+         document.querySelector(".quiz_box").classList.add("active");
+        soruGoster(quiz.soruGetir());
+})
 
-    if(quiz.sorular.length != quiz.soruIndex){
-        document.querySelector(".quiz_box").classList.add("active");
-        console.log(quiz.soruGetir());
+document.querySelector(".next_btn").addEventListener("click" , function(){
+    if(quiz.sorular.length != quiz.soruIndex+1){
         quiz.soruIndex+=1;
+        soruGoster(quiz.soruGetir());
+        
     }
     else {
         console.log("quiz bitti");
     }
-  
+
 })
 
 
+function soruGoster (soru) {
+    let question = 
+    `<span>${soru.soruMetni}</span>
+    `
+
+    let options = ''; 
+
+    for ( let cevap in soru.cevapSecenekleri) {
+        options += `
+        <div class="options">
+            <span><b>${cevap} : ${soru.cevapSecenekleri[cevap]}</b></span>
+            <div class="icon"> <i class="fas fa-check"></i></div>
+          </div>
+        `
+    }
+
+    document.querySelector(".question_text").innerHTML = question ;
+    document.querySelector(".option_list").innerHTML = options ;
+
+}
