f69d341abff53d89a116cacf61c4972c27bd64f1
Lezgin Tekay <tekay.lezgin@yandex.com>
2023-05-26 12:04:12 +0300

    control of answer options

--- script.js
+++ script.js
@@ -39,12 +39,14 @@ const quiz = new Quiz(sorular) ;
 document.querySelector(".btn_start").addEventListener("click" , function(){
          document.querySelector(".quiz_box").classList.add("active");
         soruGoster(quiz.soruGetir());
+        document.querySelector(".next_btn").classList.remove("show");
 })
 
 document.querySelector(".next_btn").addEventListener("click" , function(){
     if(quiz.sorular.length != quiz.soruIndex+1){
         quiz.soruIndex+=1;
         soruGoster(quiz.soruGetir());
+        document.querySelector(".next_btn").classList.remove("show");
         
     }
     else {
@@ -53,6 +55,9 @@ document.querySelector(".next_btn").addEventListener("click" , function(){
 
 })
 
+const option_list =  document.querySelector(".option_list");
+const correctIcon = '<div class="icon"><i class="fas fa-check"></i></div>';
+const incorrectIcon = '<div class="icon"><i class="fas fa-times"></i></div>';
 
 function soruGoster (soru) {
     let question = 
@@ -63,14 +68,37 @@ function soruGoster (soru) {
 
     for ( let cevap in soru.cevapSecenekleri) {
         options += `
-        <div class="options">
-            <span><b>${cevap} : ${soru.cevapSecenekleri[cevap]}</b></span>
-            <div class="icon"> <i class="fas fa-check"></i></div>
+        <div class="option">
+            <span><b>${cevap}</b> : ${soru.cevapSecenekleri[cevap]}</span>  
           </div>
         `
     }
 
-    document.querySelector(".question_text").innerHTML = question ;
-    document.querySelector(".option_list").innerHTML = options ;
 
+    document.querySelector(".question_text").innerHTML = question ;
+    
+    option_list.innerHTML = options ;
+
+    const option = option_list.querySelectorAll(".option");
+
+    for(let opt of option) {
+        opt.setAttribute("onclick" , "optionSelected(this)")
+    }
+}
+
+function optionSelected(option){
+    let cevap = option.querySelector("span b").textContent;
+    let soru = quiz.soruGetir();
+    if(soru.cevabiKontrolEt(cevap)){
+        option.classList.add("correct");
+        option.insertAdjacentHTML("beforeend", correctIcon);
+    }else{
+        option.classList.add("incorrect");
+        option.insertAdjacentHTML("beforeend", incorrectIcon);
+    }
+
+    for (let i=0 ; i< option_list.children.length; i++) {
+        option_list.children[i].classList.add("disabled");
+    }
+    document.querySelector(".next_btn").classList.add("show");
 }
