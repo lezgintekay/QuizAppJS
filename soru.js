fdd50ed7333c4ca073e69ee35bb7146a01f6f429
Lezgin Tekay <tekay.lezgin@yandex.com>
2023-06-12 22:58:10 +0300

    new questions added

--- soru.js
+++ soru.js
@@ -13,7 +13,7 @@ Soru.prototype.cevabiKontrolEt = function (cevap) {
 let sorular = 
 [
     new Soru("Hangisi js paket yönetim uygulamasıdır?",{a: "Node.js", b: "TypeScrript", c: "npm", d: "nuget"}, "c" ),
-    new Soru("Hangisi bles paket yönetim uygulamasıdır?",{a: "Node.js", b: "TypeScrript", c: "npm"}, "c" ),
-    new Soru("Hangisi kles paket yönetim uygulamasıdır?",{a: "Node.js", b: "TypeScrript", c: "npm"}, "c" ),
-    new Soru("Hangisi cles paket yönetim uygulamasıdır?",{a: "Node.js", b: "TypeScrript", c: "npm"}, "c" )
+    new Soru("Native Android uygulama geliştirmek için hangi programlama dili kullanılır?",{a: "Python", b: "Kotlin", c: "C#", d: "C++"}, "b" ),
+    new Soru("Aşağıdaki programlama dillerinden hangisi backend olarak değerlendirilir?",{a: "PHP", b: "TypeScrript", c: "Angular", d: "React"}, "a" ),
+    new Soru("Hangisi javascript programlama dilini kullanmaz?",{a: "Node.js", b: "React", c: "Vue,js",d: "Typescript"}, "d" )
 ];
